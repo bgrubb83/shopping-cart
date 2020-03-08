1c9785d9513917ba7637b1e52599a2c308d5b943
Barry Grubb <bgrubb83@gmail.com>
2020-03-08 13:25:09 +0000

    Add unit test suite for App component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-xit('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  render(<App />);
 });
+
+it('renders the Shop link', () => {
+  const { getByText } = render(<App />);
+  const shopLinkElement = getByText(/Shop/);
+  expect(shopLinkElement).toBeInTheDocument();
+});
+
+it('renders the Add to Cart buttons', () => {
+  const { getAllByText } = render(<App />);
+  const CartLinkElement = getAllByText(/Add to Cart/);
+  expect(CartLinkElement).not.toBeUndefined();
+});
+
